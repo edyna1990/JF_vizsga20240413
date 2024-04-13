bfadc82d7979165970a1eccc5bc76041c80570c8
Adam Takacs <takacs.adam@webdream.hu>
2024-04-13 12:57:58 +0200

    readme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,30 +7,30 @@ export const Home = () => {
       <header>
         <h1>Ingatlaniroda</h1>
       </header>
-      <main className="row home-image">
-          <div className="col-12 col-md-4">
-            <h4>Ingatlanok minden kategóriában</h4>
-          </div>
-          <div className="col-md-4 col-12">
-            <Card style={{width: "18rem"}} className="home-card">
-              <img alt="haz" src="haz.jpg" />
-              <CardBody>
-                <CardTitle tag="h5">Házak</CardTitle>
-              </CardBody>
-            </Card>
-            <Card style={{width: "18rem"}} className="home-card">
-              <img alt="lakas" src="panel.jpg" />
-              <CardBody>
-                <CardTitle tag="h5">Lakások</CardTitle>
-              </CardBody>
-            </Card>
-            <Card style={{width: "18rem"}} className="home-card">
-              <img alt="nyaralo" src="nyaralo.jpg" />
-              <CardBody>
-                <CardTitle tag="h5">Nyaralók</CardTitle>
-              </CardBody>
-            </Card>
-          </div>
+      <main className="row">
+        <div className="col-12 col-md-4">
+          <h4>Ingatlanok minden kategóriában</h4>
+        </div>
+        <div className="row col-md-4 col-12 home-image">
+          <Card >
+            <img alt="haz" src="haz.jpg" />
+            <CardBody>
+              <CardTitle tag="h5">Házak</CardTitle>
+            </CardBody>
+          </Card>
+          <Card>
+            <img alt="lakas" src="panel.jpg" />
+            <CardBody>
+              <CardTitle tag="h5">Lakások</CardTitle>
+            </CardBody>
+          </Card>
+          <Card>
+            <img alt="nyaralo" src="nyaralo.jpg" />
+            <CardBody>
+              <CardTitle tag="h5">Nyaralók</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
       </main>
       <footer>
         ©2024 JF project. All rights reserved.
